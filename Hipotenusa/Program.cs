022725b306cb3e212c7f5636c2572fb48c09393f
masfazan <msfazan@icloud.com>
2024-04-21 16:08:24 -0300

    Add project files.

---
+++ Hipotenusa/Program.cs
@@ -0,0 +1,15 @@
+﻿double cat1=0, cat2=0, hipotenusa=0;
+
+Console.WriteLine("Digite o valor do cateto1: ");
+cat1=double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o cateto 2: ");
+cat2= double.Parse(Console.ReadLine());
+
+hipotenusa=Math.Sqrt(cat1*cat1+cat2*cat2);
+
+Console.WriteLine("A hipotenusa é: " + hipotenusa);
+
+
+
+
